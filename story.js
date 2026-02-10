61d36f16ba7a3af592c45c9e27be08e47d6ffb5b
Demak-o <demakanator8@gmail.com>
2026-02-11 10:34:03 +1100

    Update story.js

--- story.js
+++ story.js
@@ -1,13 +1,17 @@
 const story = {
   start: {
-    text: "You wake up in a dark forest. You hear something moving in the trees.",
+    text: "As your eyes open you look around the darkend unlit room. That was your last night in the rented room of the tavern, you get up and head over to the door. You8 are staying in a town called Björnsfield. You open the heavy wooden door and get blinded by the vibrant town. As your eyes adjust to the early morning light you become aware of the people pushing past you on the street, looking directly across the pebbled streets you see more medieval style houses line the street on both sides. Looking down the street to the left and see more houses and shops lining the streets curbs; to your right you see a large stone building. Do you choose to go down the road or up the street to the large stone building.",
     choices: [
-      { text: "Investigate the sound", next: "investigate" },
-      { text: "Run away", next: "run" }
+      { text: "Head down the street", next: "down" },
+      { text: "Head up the street", next: "up" }
     ]
   },
+  up: {
+    text: "you die lol, cancer or smth.",
+    choices: []
+  },
 
-  investigate: {
+  down: {
     text: "A wolf steps into the clearing, staring at you.",
     choices: [
       { text: "Fight the wolf", next: "fight" },
